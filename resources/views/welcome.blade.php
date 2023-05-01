03c981a6d164146256f422d554b7603e99f379f8
Jackson Zabala <jacksonzabala@libero.it>
2023-05-01 21:24:57 +0200

    complet feedback zone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,6 @@
         <div class="col-12 col-lg-4 backgroundBike d-flex justify-content-center">
             <div class="mt-2 descriptionTreeCard">
                     <p class="textColor text-center generalText"><span class="fs-2">Biciclette </span>I migliori marchi sul mercato</p>
-                    {{-- <p class="textColor text-center">I migliori marchi sul mercato</p> --}}
                 <div class=" d-flex justify-content-center">
                     <a href="{{route('bike.index')}}">
                         <button class="button generalText">Scopri di più</button>
@@ -84,7 +83,7 @@ space-between="30" free-mode="true">
 
 
 
-
+{{-- --------carosel bikes --}}
 <swiper-container class="mySwiper pb-5" pagination="true" pagination-clickable="true" slides-per-view="2"
 space-between="30" free-mode="true">
 @foreach($bikes as $bike)
@@ -92,7 +91,6 @@ space-between="30" free-mode="true">
     <div>
         <img src="{{Storage::url($bike->photo)}}" class="card-img-top img-fluid" alt="...">
         <h3>{{$bike->brand}}</h3>
-        <p>{{$bike->name}}</p>
         <a href="{{route('bike.show', compact('bike'))}}">
             <button class="button generalText text-dark">Dettagli</button>
         </a>
@@ -102,7 +100,83 @@ space-between="30" free-mode="true">
 </swiper-slide>
 @endforeach
 </swiper-container>
+
+{{-- ----------carosel spare --}}
+    
+<div class="separationGradient"></div>
+<div class="container mt-5">
+    <div class="row">
+        <div class="text-center">
+            <img id="logo" src="/media/logoArancio.png" alt="">
+            <h3 class="fs-1 generalText fw-bold">Ricambi</h3>
+            <p class="brandFont fs-3 text-muted">Gli ultimi ricambi inseriti</p>
+        </div>
+    </div>
+</div>
+<swiper-container class="mySwiper pb-5" pagination="true" pagination-clickable="true" slides-per-view="2"
+space-between="30" free-mode="true">
+@foreach($spares as $spare)
+<swiper-slide>
+    <div>
+        <img src="{{Storage::url($spare->photo)}}" class="card-img-top img-fluid" alt="...">
+        <p class="fst-italic">Marchio</p>
+        <h3>{{$spare->brand}}</h3>
+        <a href="{{route('spare.show', compact('spare'))}}">
+            <button class="button generalText text-dark">Dettagli</button>
+        </a>
+        {{-- <p class="fst-italic text-muted">{{$bike->user->name}}</p> --}}
+        
+    </div>
+</swiper-slide>
+@endforeach
+</swiper-container>
+
+{{-- --------extra carosel --}}
+<div class="separationGradient"></div>
+<div class="container mt-5">
+    <div class="row">
+        <div class="text-center">
+            <img id="logo" src="/media/logoArancio.png" alt="">
+            <h3 class="fs-1 generalText fw-bold">Cerca tra gli accessori</h3>
+            <p class="brandFont fs-3 text-muted">Compatibili con diversi marchi e modelli</p>
+        </div>
+    </div>
+</div>
+<swiper-container class="mySwiper pb-5" pagination="true" pagination-clickable="true" slides-per-view="2"
+space-between="30" free-mode="true">
+@foreach($extras as $extra)
+<swiper-slide>
+    <div>
+        <img src="{{Storage::url($extra->photo)}}" class="card-img-top img-fluid" alt="...">
+        <h3>{{$extra->brand}}</h3>
+        <a href="{{route('extra.show', compact('extra'))}}">
+            <button class="button generalText text-dark">Dettagli</button>
+        </a>
+        {{-- <p class="fst-italic text-muted">{{$bike->user->name}}</p> --}}
+        
+    </div>
+</swiper-slide>
+@endforeach
+</swiper-container>
 {{-- END CAROSEL --}}
+
+{{-- START FEEDBACK --}}
+<section class="feedback">
+    <div class="text-center">
+        <img class="mt-5" id="logo" src="/media/logoArancio.png" alt="">
+        <h3 class="fs-1 generalText text-white fw-bold">Su di noi</h3>
+        <p class="brandFont fs-3 text-muted">Clienti felici</p>
+    </div>
+    <div class="containerBlurFeedBack feedBackBlur mx-auto d-flex align-items-center">
+        <div class="p-3">
+            <h3 class="generalText textColor fontSizeFeedBack">
+                "La mia nuova bici acquistata su questo sito è fantastica! Perfetta per le mie escursioni in montagna e dal design moderno e accattivante. Consiglio vivamente!"
+            </h3>
+            <p class="fst-italic fw-lighter text-white fs-4">Michele.</p>
+        </div>
+    </div>
+</section>
+{{-- END FEEDBACK --}}
 
 
     {{-- <x-header>  
@@ -115,7 +189,7 @@ space-between="30" free-mode="true">
         </div>
     @endif
     
-    <div class="container-fluid background">
+    {{-- <div class="container-fluid background">
         <div class="row justify-content-center">
 
             @foreach($bikes as $bike)
@@ -133,7 +207,6 @@ space-between="30" free-mode="true">
                                 <div class="solu_description">
                                     <p>{{$bike->name}}</p>
                                     <p class="fst-italic text-muted">{{$bike->user->name}}</p>
-                                    {{-- @dd($bike->user->name) --}}
                                     <a href="{{route('bike.show', compact('bike'))}}">
                                         <button type="button" class="read_more_btn">Dettagli</button>
                                     </a>
@@ -163,7 +236,6 @@ space-between="30" free-mode="true">
                                         <p>{{$spare->brand}}</p>
                                         <p>{{$spare->description}}</p>
                                         <p class="fst-italic text-muted">{{$spare->user->name}}</p>
-                                        {{-- @dd($bike->user->name) --}}
                                         <a href="{{route('spare.show', compact('spare'))}}">
                                             <button type="button" class="read_more_btn">Dettagli</button>
                                         </a>
@@ -175,7 +247,7 @@ space-between="30" free-mode="true">
             @endforeach
 
         </div></div>
-    </div>
+    </div> --}}
 {{-- ANIMAZIONE --}}
 <div class="camion d-flex">
     ciao
